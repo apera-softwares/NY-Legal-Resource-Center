3d246082e0f060ff7668849b40f661cbd855bd61
Sonawane-Shubham <sssonawane047@gmail.com>
2025-07-15 14:49:02 +0530

    centralize law sections colors

--- src/components/labor-laws/LaborLawItem.tsx
+++ src/components/labor-laws/LaborLawItem.tsx
@@ -8,21 +8,21 @@ interface LaborLawItemProps {
 const LaborLawItem: React.FC<LaborLawItemProps> = ({ title, content }) => {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="w-full p-4 sm:p-6 border border-[#05588E29] rounded-xl lg:rounded-2xl space-y-4 ">
+    <div className="w-full p-4 sm:p-6 border border-border-primary rounded-xl lg:rounded-2xl space-y-4 ">
       <div
         onClick={() => setIsOpen(!isOpen)}
         className="flex items-center justify-between gap-4 cursor-pointer"
       >
         <h2
           className={`text-lg sm:text-xl lg:text-2xl font-bold ${
-            isOpen ? "text-[#3498DB]" : ""
+            isOpen ? "text-primary" : "text-text-base"
           } transition-all duration-300`}
         >
           {title}
         </h2>
         <span className="">
           <ImArrowUpRight2
-            className={` text-2xl  ${isOpen ? "text-[#3498DB] rotate-90 " : " rotate-0"} transition-all duration-300 `}
+            className={` text-2xl  ${isOpen ? "text-primary rotate-90 " : " text-text-base rotate-0"} transition-all duration-300 `}
           />
         </span>
       </div>
