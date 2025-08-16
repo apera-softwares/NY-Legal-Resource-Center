cc9bd5b57d017f2bb6f5f1a0d37ed5b023d1e6a5
pratikSondauleApera <pratik@aperasoftwares.com>
2025-08-16 15:28:21 +0530

    changed the backend url

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,3 +1,4 @@
-export const BACKEND_API_BASE_URL = "http://209.182.232.11:5022/";
+// export const BACKEND_API_BASE_URL = "http://209.182.232.11:5022/";
+export const BACKEND_API_BASE_URL = "https://api.nyprc.org/";
 
 // export const BACKEND_API_BASE_URL = "http://209.182.232.11:5021/";
